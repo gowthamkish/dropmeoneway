13f64750cd5673308b67f86b0e36511cf245f5d8
gowthamkish <gowthamkish@outlook.com>
2025-09-23 23:32:22 +0530

    Added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Header from './components/header/Header';
+import Header from "./components/header/Header";
 import HeroBooking from "./components/booking/HeroBooking";
 // import BookingForm from "./components/booking/BookingForm";
 import PopularCities from "./components/popular-cities/PopularCities";
-import Footer from './components/footer/Footer';
+import Footer from "./components/footer/Footer";
 import About from "./components/about/About";
-
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
       <br />
       <About />
       <Footer />
+      <Analytics />
     </>
   );
 }
